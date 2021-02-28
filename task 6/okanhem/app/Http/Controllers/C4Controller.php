818fb17b15a93afe6927c867b024f38678e996ab
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-02-28 14:21:43 +0700

    task 6

---
+++ task 6/okanhem/app/Http/Controllers/C4Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use  Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
+
+class C4Controller extends BaseController
+{
+    public function ok(){
+        return 'Homecontroller c4';
+    }
+}
---
+++ task 6/okanhem/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use  Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
+
+class HomeController extends BaseController
+{
+    public function showWelcome(){
+        return 'Home Controller';
+    }
+}
