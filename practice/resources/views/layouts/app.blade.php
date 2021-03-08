7a30b640582d355c32c2cb4e4c5c3d073c94ef4e
lehoangminhfpt2020 <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-03-08 10:27:05 +0700

    Update app.blade.php

--- practice/resources/views/layouts/app.blade.php
+++ practice/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pactive student</title>
+    <title>Pactice student</title>
     <style>
         table {
             font-family: arial, sans-serif;
